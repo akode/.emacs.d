73775cd59ad029fbb76f0a1706e93aa643d7fe0c
akode <andreas.kodewitz@gmail.com>
2014-11-26 22:17:08 +0100

    small update on Mac to get rid of a warning

--- config/init.el
+++ config/init.el
@@ -1,17 +1,30 @@
 ;; two variables are defined in .emacs
 ;; my-gcal-priv : address of the my google mail calendar
-;; my-org-directory : path to my org files
+;; my-org-directories : first of the list is the main directory
 
 (server-start)
 
+;; crossplatform helpers
+(defun system-is-mac ()
+  (interactive)
+  (string-equal system-type "darwin"))
+
+(defun system-is-linux ()
+  (interactive)
+  (string-equal system-type "gnu/linux"))
+
+(defun system-is-windows ()
+  (interactive)
+  (string-equal system-type "windows-nt"))
+
 ;; Load paths
-(let ((default-directory "~/.emacs.d/"))
-  (normal-top-level-add-to-load-path '("." "packages/matlab-emacs")))
+(let ((default-directory "~/.emacs.d/config/"))
+  (normal-top-level-add-to-load-path '("." "../packages/matlab-emacs" "../packages/ensime_2.10.0-0.9.8.9/elisp")))
 ;; if these packages are not present just comment matlab-mode out
 
 ;; Packages
 (defvar sys-packages
-  '(auctex auto-complete less-css-mode smartparens yasnippet markdown-mode calfw ess jinja2-mode egg elpy)
+  '(auctex auto-complete smartparens yasnippet markdown-mode less-css-mode coffee-mode calfw ess egg elpy scala-mode2)
   "List of packages installed via packages system from elpa and melpa.")
 
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
@@ -42,6 +55,7 @@
 (setq mac-command-modifier 'meta)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-cc" 'org-capture)
 (global-set-key (kbd "C-x f") 'find-file-at-point)
 
 ;;;;;;;;;;;;;;;;;;;;
@@ -65,8 +79,29 @@
 (require 'egg)
 
 ;;;;;;;;;;;;;;;;;;;;
+;; SASS css mode
+(add-to-list 'auto-mode-alist '("\\.scss\\'" . sass-mode))
+
+;;;;;;;;;;;;;;;;;;;;
+;; Coffee script mode
+(add-to-list 'auto-mode-alist '("\\.coffee\\'" . coffee-mode))
+
+;;;;;;;;;;;;;;;;;;;;
+;; Web mode
+(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+
+;;;;;;;;;;;;;;;;;;;;
+;; Ensime/Scala
+(autoload 'ensime "ensime" "Ensime Scala mode." t)
+(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+
+;;;;;;;;;;;;;;;;;;;;
 ;; Matlab-mode
-(require 'matlab-load)
+;(require 'matlab-load)
+
+;;;;;;;;;;;;;;;;;;;;
+;; org-pandoc
+(require 'ox-pandoc)
 
 ;;;;;;;;;;;;;;;;;;;;
 ;; Python
@@ -78,13 +113,11 @@
 (autoload 'markdown-mode "markdown-mode" "Markdown mode." t)
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
-;;;;;;;;;;;;;;;;;;;;
-;; ESS
-(require 'ess-site)
+(add-to-list 'auto-mode-alist '("\\.html.org\\'" . org-mode))
 
 ;;;;;;;;;;;;;;;;;;;;
-;; Jinja2
-(autoload 'jinja2-mode "jinja2-mode" "Jinja2 mode." t)
+;; ESS
+;(require 'ess-site)
 
 ;;;;;;;;;;;;;;;;;;;;
 ;; Autocomplete
@@ -111,7 +144,7 @@
 
 (add-hook 'LaTeX-mode-hook 'yas-minor-mode)
 (add-hook 'rst-mode-hook 'yas-minor-mode)
-(add-hook 'html-mode-hook 'yas-minor-mode)
+(add-hook 'web-mode-hook 'yas-minor-mode)
 
 ;;;;;;;;;;;;;;;;;;;;
 ;; IDO
@@ -121,28 +154,14 @@
 (ido-mode t)
 
 ;;;;;;;;;;;;;;;;;;;;
-;; Calendar/calfw
-(require 'calfw)
-(require 'calfw-org)
-(require 'calfw-ical)
-(defun open-calendar ()
-  (interactive)
-  (cfw:open-calendar-buffer
-   :contents-sources
-   (list
-    (cfw:org-create-source "Green")
-    (cfw:ical-create-source "gcal" my-gcal-priv "IndianRed")
-    )))
-
-(load "calendar-cust")
-
-;;;;;;;;;;;;;;;;;;;;
 ;; OrgMode
 (autoload 'org-latex "org-latex" t)
-(setq org-directory my-org-directory)
+(setq org-directory (car my-org-directories))
 (setq org-default-notes-file (concat org-directory "/notes.org"))
 (setq diary-file (concat org-directory "/.diary"))
-(setq org-agenda-files (list my-org-directory))
+(setq org-agenda-files my-org-directories)
+
+(setq org-refile-targets (quote ((org-agenda-files :maxlevel . 9))))
 
 (setq org-modules (quote (org-habit
 			  org-bibtex
